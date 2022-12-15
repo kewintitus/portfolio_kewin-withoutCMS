de21beb1a956fa8b1e33309c46148867a69b5c81
Kewin Titus <kewintitus@gmail.com>
2022-12-15 18:05:08 +0530

    added skill labels

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -4,17 +4,22 @@ import { motion } from 'framer-motion';
 type Props = {
   svg: any;
   directionLeft?: boolean;
+  name: String;
 };
 
-function Skill({ svg, directionLeft }: Props) {
+function Skill({ svg, directionLeft, name }: Props) {
   return (
-    <div>
+    <div className="flex flex-col">
       <motion.div
         initial={{ x: directionLeft ? -100 : 100, opacity: 0 }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, x: 0 }}
+        className="flex flex-col justify-center justify-self-end"
       >
-        {svg}
+        <div className="self-center justify-self-center">
+          {svg}
+          <p className="text-center text-gray-400">{name}</p>
+        </div>
       </motion.div>
     </div>
   );
